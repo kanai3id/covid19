d76e634e2388cc05a90a06e3d96227ac619519a7
kanai3id <kanai3id@gmail.com>
2020-04-03 23:15:19 +0900

    Changed to Nagano data format.

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -1,28 +1,31 @@
-import dayjs from 'dayjs'
+// import dayjs from 'dayjs'
 
 const headers = [
-  { text: '公表日', value: '公表日' },
+  { text: '日付', value: '日付' },
   { text: '居住地', value: '居住地' },
-  { text: '年代', value: '年代' },
+  { text: '年齢', value: '年齢' },
   { text: '性別', value: '性別' },
-  { text: '退院※', value: '退院', align: 'center' }
+  { text: '状況※', value: '状況', align: 'center' },
+  { text: '接触者', value: '接触者' }
 ]
 
 type DataType = {
-  リリース日: string
+  日付: string
   居住地: string | null
-  年代: string | null
+  年齢: string | null
   性別: '男性' | '女性' | string
-  退院: '◯' | null
+  状況: string | null
+  接触者: string | null
   [key: string]: any
 }
 
 type TableDataType = {
-  公表日: string
+  日付: string
   居住地: DataType['居住地']
-  年代: DataType['年代']
+  年齢: DataType['年齢']
   性別: DataType['性別'] | '不明'
-  退院: DataType['退院']
+  状況: DataType['状況']
+  接触者: DataType['接触者']
 }
 
 type TableDateType = {
@@ -42,11 +45,12 @@ export default (data: DataType[]) => {
   }
   data.forEach(d => {
     const TableRow: TableDataType = {
-      公表日: dayjs(d['リリース日']).format('MM/DD') ?? '不明',
+      日付: d['日付'],
       居住地: d['居住地'] ?? '調査中',
-      年代: d['年代'] ?? '不明',
+      年齢: d['年齢'] ?? '不明',
       性別: d['性別'] ?? '不明',
-      退院: d['退院']
+      状況: d['状況'],
+      接触者: d['接触者']
     }
     tableDate.datasets.push(TableRow)
   })
