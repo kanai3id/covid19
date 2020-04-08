40b31fd58cd0dcf48b9ef3a762de1fc17ba9ac07
kanai3id <kanai3id@gmail.com>
2020-04-09 08:28:33 +0900

    Added two new cards (positive person status graph and hospital stay days).

---
+++ components/cards/HospitalizationCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <time-bar-chart
+      :title="$t('入退院数')"
+      :title-id="'hospitalization'"
+      :chart-id="'time-bar-chart-hospitalization'"
+      :chart-data="hospitalizationGraph"
+      :date="hospitalization.last_update"
+      :unit="'人'"
+      :url="
+        'https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/corona-doko.html'
+      "
+      :show-button="false"
+    />
+  </v-col>
+</template>
+
+<script>
+import hospitalization from '@/data/hospitalization.json'
+import TimeBarChart from '@/components/TimeBarChart.vue'
+import formatVariableGraph from '@/utils/formatVariableGraph'
+
+export default {
+  components: {
+    TimeBarChart
+  },
+  data() {
+    // 入退院数
+    const hospitalizationGraph = formatVariableGraph(hospitalization.data)
+
+    const data = {
+      hospitalization,
+      hospitalizationGraph
+    }
+    return data
+  }
+}
+</script>
---
+++ components/cards/StayCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <time-bar-chart
+      :title="$t('入院日数')"
+      :title-id="'stay'"
+      :chart-id="'time-bar-chart-stay'"
+      :chart-data="stayGraph"
+      :date="stay.last_update"
+      :unit="'人'"
+      :url="
+        'https://www.pref.nagano.lg.jp/hoken-shippei/kenko/kenko/kansensho/joho/corona-doko.html'
+      "
+      :show-button="false"
+    />
+  </v-col>
+</template>
+
+<script>
+import stay from '@/data/stay.json'
+import TimeBarChart from '@/components/TimeBarChart.vue'
+import formatVariableGraph from '@/utils/formatVariableGraph'
+
+export default {
+  components: {
+    TimeBarChart
+  },
+  data() {
+    // 入退日数
+    const stayGraph = formatVariableGraph(stay.data)
+
+    const data = {
+      stay,
+      stayGraph
+    }
+    return data
+  }
+}
+</script>
