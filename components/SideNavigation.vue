3ed814a1a2e0ff3eb9d4040b9918ce7d3d847ee8
kanai3id <kanai3id@gmail.com>
2020-04-08 14:05:11 +0900

    Added Nagano Corona virus Twitter account.

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -63,17 +63,6 @@
             </picture>
           </a>
           <a
-            href="https://twitter.com/tokyo_bousai"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="SideNavigation-SocialLink"
-          >
-            <picture>
-              <source srcset="/twitter.webp" type="image/webp" />
-              <img src="/twitter.png" alt="Twitter" />
-            </picture>
-          </a>
-          <a
             href="https://www.facebook.com/tochokoho"
             target="_blank"
             rel="noopener noreferrer"
@@ -94,6 +83,17 @@
             <picture>
               <source srcset="/line.webp" type="image/webp" />
               <img src="/line.png" alt="LINE" />
+            </picture>
+          </a>
+          <a
+            href="https://twitter.com/Nagano_Corona"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="SideNavigation-SocialLink"
+          >
+            <picture>
+              <source srcset="/twitter.webp" type="image/webp" />
+              <img src="/twitter.png" alt="Twitter" />
             </picture>
           </a>
           <a
