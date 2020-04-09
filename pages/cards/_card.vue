84e7ab4c5db538fa201a44cfcb46e9c9cdbed9ce
kanai3id <kanai3id@gmail.com>
2020-04-09 17:32:34 +0900

    Changed card description from "長野県が開設" to "長野県内の個人が開設"

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -154,7 +154,7 @@ export default {
         ? `${url}/ogp/${this.$route.params.card}.png?t=${timestamp}`
         : `${url}/ogp/${this.$i18n.locale}/${this.$route.params.card}.png?t=${timestamp}`
     const description = `${this.updatedAt} | ${this.$t(
-      '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、長野県が開設したものです。'
+      '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、長野県内の個人が長野県非公式で開設したものです。'
     )}`
 
     return {
