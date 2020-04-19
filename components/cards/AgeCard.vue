865aa41ef0fa390e7e5b7fa56266e5aac73afb55
kanai3id <kanai3id@gmail.com>
2020-04-19 15:40:20 +0900

    Refactored age and hospitalization stay cards.

---
+++ components/cards/AgeCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <age-bar-chart
+      :title="$t('年代別陽性患者数')"
+      :title-id="'age-card'"
+      :chart-id="'chart-age-card'"
+      :chart-data="ageData"
+      :date="ageData.date"
+      :unit="$t('人')"
+    >
+      <template v-slot:description>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </template>
+    </age-bar-chart>
+  </v-col>
+</template>
+
+<script>
+import ageData from '@/data/age.json'
+import AgeBarChart from '@/components/AgeBarChart.vue'
+export default {
+  components: {
+    AgeBarChart
+  },
+  data() {
+    return {
+      ageData
+    }
+  }
+}
+</script>
