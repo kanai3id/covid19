8100f2d1aa326fddb7b627653b7b142f781e57db
kanai3id <kanai3id@gmail.com>
2020-04-20 15:37:50 +0900

    Change parameter name from "接触者数" to "滞在歴等".

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -6,7 +6,7 @@ const headers = [
   { text: '年代', value: '年代' },
   { text: '性別', value: '性別' },
   { text: '状況', value: '状況', align: 'center' },
-  { text: '接触者', value: '接触者' }
+  { text: '滞在歴等', value: '滞在歴等' }
 ]
 
 type DataType = {
@@ -15,7 +15,7 @@ type DataType = {
   年代: string | null
   性別: '男性' | '女性' | string
   状況: string | null
-  接触者: string | null
+  滞在歴等: string | null
   [key: string]: any
 }
 
@@ -25,7 +25,7 @@ type TableDataType = {
   年代: DataType['年代']
   性別: DataType['性別'] | '不明'
   状況: DataType['状況']
-  接触者: DataType['接触者']
+  滞在歴等: DataType['滞在歴等']
 }
 
 type TableDateType = {
@@ -50,7 +50,7 @@ export default (data: DataType[]) => {
       年代: d['年代'] ?? '不明',
       性別: d['性別'] ?? '不明',
       状況: d['状況'],
-      接触者: d['接触者']
+      滞在歴等: d['滞在歴等']
     }
     tableDate.datasets.push(TableRow)
   })
