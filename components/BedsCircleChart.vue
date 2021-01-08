d247f80366851004b6f05169545131377a2ace12
kanai3id <kanai3id@gmail.com>
2021-01-09 08:42:58 +0900

    Latest bed information from the Ministry of Health, Labor and Welfare.

--- components/BedsCircleChart.vue
+++ components/BedsCircleChart.vue
@@ -4,11 +4,11 @@
       <ul :class="$style.notes">
         <li>
           <external-link
-            url="https://www.mhlw.go.jp/content/10900000/000714776.pdf"
+            url="https://www.mhlw.go.jp/content/10900000/000717132.pdf"
           >
             {{
               $t('厚生労働省:療養状況等及び入院患者受入病床数について')
-            }}(2020-12-30)
+            }}(2021-1-6)
           </external-link>
         </li>
       </ul>
