9389d96328bd8b79d0c63964659f0c8b7869f0b6
kanai <kanai@kanaiMac.local>
2021-02-15 11:53:43 +0900

    ogp peformance problem

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -28,7 +28,7 @@ options.add_argument("--hide-scrollbars")
 
 driver = webdriver.Chrome(options=options)
 
-for lang in ("ja", "en"):
+for lang in ("ja"):
     if not os.path.exists("ogp/{}".format(lang)):
         os.mkdir("ogp/{}".format(lang))
     for path, size in PATHS.items():
