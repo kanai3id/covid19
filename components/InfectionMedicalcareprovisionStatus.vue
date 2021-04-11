121b2c76996fea5d94b7665e2bb638d71cf72bb0
kanai3id <kanai3id@gmail.com>
2021-04-11 14:05:44 +0900

    Added rate of increase in staying population around Nagano Station.

--- components/InfectionMedicalcareprovisionStatus.vue
+++ components/InfectionMedicalcareprovisionStatus.vue
@@ -2,7 +2,7 @@
   <div class="InfectionMedicalcareprovisionStatus">
     <div class="InfectionMedicalcareprovisionStatus-heading">
       <h3 class="InfectionMedicalcareprovisionStatus-title">
-        {{ $t('感染状況・医療提供体制') }}
+        {{ $t('感染状況・医療提供体制・長野駅周辺人口増減状況') }}
         {{ date }}時点
       </h3>
     </div>
@@ -34,6 +34,20 @@
         }}<span>{{ statuses.data['入院数'].toLocaleString() }}人</span> （{{
           $t('確保病床数')
         }}<span>{{ statuses.data['確保病床数'].toLocaleString() }}床</span>）
+      </div>
+    </div>
+    <div class="InfectionMedicalcareprovisionStatus-Box">
+      <div class="InfectionMedicalcareprovisionStatus-Headline">
+        <app-link to="https://mobaku.jp/covid-19/archive/kantokoshinetsu.html#area_31">
+          {{ $t('長野駅増減率') }}
+        </app-link>
+      </div>
+      <div class="InfectionMedicalcareprovisionStatus-description">
+        {{ $t('滞在人口増減率')
+        }}<span> {{ statuses.data['滞在人口増減率'].toLocaleString() }}%</span> （{{
+          $t('計測日')
+        }}<span>{{ statuses.data['滞在人口増減日時'].toLocaleString() }}</span>）
+        {{ $t('出典元:NTTドコモ モバイル空間統計') }}
       </div>
     </div>
   </div>
