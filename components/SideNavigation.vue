b17245eaf1ec53600bc76ef79f584929f8ddd482
kanai3id <kanai3id@gmail.com>
2021-05-04 09:07:11 +0900

    Change title "県内" to "長野"

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -155,7 +155,7 @@ export default Vue.extend({
       return [
         {
           icon: 'mdi-chart-timeline-variant',
-          title: this.$t('県内の最新感染動向'),
+          title: this.$t('長野の最新感染動向'),
           link: this.localePath('/')
         },
         {
