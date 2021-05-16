c27e99ce8ba70dd8cf661c0dbb13fe7f712ce97c
kanai <kanai@kanaiMac.local>
2021-05-16 19:09:57 +0900

    Optimize

--- utils/i18nUtils.ts
+++ utils/i18nUtils.ts
@@ -10,9 +10,9 @@ export const getLinksLanguageAlternative = (
   const getFullPathWihLocale = (locale: string) => {
     const pathLocale = locale === 'ja' ? '' : `/${locale}`
     if (routeBaseName === 'index') {
-      return `https://stopcovid19.metro.tokyo.lg.jp${pathLocale}`
+      return `https://covid19-nagano.info${pathLocale}`
     } else {
-      return `https://stopcovid19.metro.tokyo.lg.jp${pathLocale}/${routeBaseName}`
+      return `https://covid19-nagano.info${pathLocale}/${routeBaseName}`
     }
   }
 
